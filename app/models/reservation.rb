e723904b97b48e6ff04f1a873fcb207b8a940496
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2023-08-11 18:25:05 -0300

    Add validations to models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :site
+
+  validates :name, presence: true
+  validates :start, presence: true
+  validates :end, presence: true
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,7 @@
 class Site < ApplicationRecord
   has_many :reservations
+
+  validates :name, presence: true
+  validates :number, presence: true
+  validates :beds, presence: true
 end
