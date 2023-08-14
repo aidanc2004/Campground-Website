4ab61c5961fe1919e0f2d1724d7f23b57b88ac0e
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2023-08-14 20:16:59 -0300

    Make sure reservations can't overlap

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -13,10 +13,14 @@ class ReservationsController < ApplicationController
   def create
     @site = Site.find(params[:site_id])
     @reservation = @site.reservations.new(reservation_params)
+
+    # What user this reservation is for
     @reservation.user_id = current_user.id
+    # Total price of reservation
     @reservation.total = helpers.get_total @reservation
 
-    if @reservation.save
+    # Make sure that this doesn't overlap with another reservation
+    if helpers.doesnt_overlap?(@reservation) && @reservation.save
       redirect_to site_reservation_path(id: @reservation.id)
     else
       render :new, status: :unprocessable_entity
--- app/helpers/reservation_helper.rb
+++ app/helpers/reservation_helper.rb
@@ -13,4 +13,23 @@ module ReservationHelper
     # Get reservation total and round it to two decimal places
     total = (nights * price * tax_rate).round(2, :half_even);
   end
+
+  # Makes sure two reservations don't overlap
+  def doesnt_overlap?(reservation)
+    site = reservation.site
+    
+    site.reservations.each do |r|
+      # If this is the same reservation
+      if reservation == r
+        next
+      end
+      # NOT <= and >= because a user can check in the same day another checks out
+      if reservation.start < r.end && reservation.end > r.start
+        flash[:alert] = "Site is not avaliable during these days"
+        return false
+      end
+    end
+    
+    true
+  end
 end
