2579ca7a751e4a9bac5e563109c9c36878f9e9cf
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2023-08-17 23:22:36 -0300

    Allow admin to create and remove sites

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,4 +2,50 @@ class SitesController < ApplicationController
   def sites
     @sites = Site.all
   end
+
+  def show
+    @site = Site.find(params[:id])
+  end
+
+  def new
+    # Make sure user is an admin
+    if !current_user.is_admin?
+      flash[:alert] = "Must be an admin to view this page."
+      redirect_to :root
+    end
+
+    @site = Site.new
+  end
+
+  def create
+    # Make sure user is an admin
+    if !current_user.is_admin?
+      flash[:alert] = "Must be an admin to view this page."
+      redirect_to :root
+    end
+
+    @site = Site.new(site_params)
+
+    if @site.save
+      redirect_to sites_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # Make sure user is an admin
+    if !current_user.is_admin?
+      flash[:alert] = "Must be an admin to view this page."
+      redirect_to :root
+    end
+
+    @site = Site.find(params[:id])
+    @site.destroy
+  end
+
+  private
+    def site_params
+      params.require(:site).permit(:name, :number, :beds, :price)
+    end
 end
